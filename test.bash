7e967a70c9305cc0d4c54b7e6656d445f851efcf
Yudai Shimizu <yudai19891023@gmail.com>
2024-08-03 00:30:00 +0900

    Add tests

--- test.bash
+++ test.bash
@@ -13,5 +13,14 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng $LINENO
 
+### STRANGE INPUT ###
+out=$(echo a | ./plus)
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
 [ "$res" = 0 ] && echo OK
 exit $res
