c63fec6828cde3f80fe5579493ac888fae801be8
Yudai Shimizu <hoge@gmail.com>
2025-02-19 23:38:17 -0800

    Improve the test

--- test.bash
+++ test.bash
@@ -1,5 +1,16 @@
 #!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Yudai
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng $LINENO
 
-[ "${out}" = 15 ]
+[ "$res" = 0 ] && echo OK
+exit $res
