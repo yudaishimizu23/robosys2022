8195bf35c94ed899e64e66f8ecb4f04e49adf4f8
Yudai Shimizu <hoge@gmail.com>
2025-02-21 22:39:35 -0800

    Add tests

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2025 Yudai
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -8,9 +8,19 @@ ng () {
 }
 
 res=0
+
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng $LINENO
+
+## STRANGE INPUT ###
+out=$(echo a | ./plus)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res
